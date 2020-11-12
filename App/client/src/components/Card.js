3034c85b4cf824ccad4ef6b3b5057bd2e4f3444a
Dillon Long <dplong@crimson.ua.edu>
2020-11-12 13:25:27 -0800

    Added a favorite button the user can click on each card

--- App/client/src/components/Card.js
+++ App/client/src/components/Card.js
@@ -1,6 +1,9 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { useHistory } from 'react-router-dom';
 import styled from 'styled-components';
+import {FontAwesomeIcon} from '@fortawesome/react-fontawesome';
+import {faHeart as fasHeart} from '@fortawesome/free-solid-svg-icons';
+import {faHeart as farHeart} from '@fortawesome/free-regular-svg-icons';
 
 const CardWrapper = styled.div`
   background-color: #fc6701;
@@ -10,23 +13,42 @@ const CardWrapper = styled.div`
   border-radius: 30px;
   border: 1px solid #fecc3d;
   display: flex;
-  cursor: pointer;
   flex-direction: column;
   justify-content: center;
 
+  .info {
+    align-self: center;
+    display: flex;
+    width: 80%;
+    justify-content: center;
+    height: 30px;
+  }
+
   .name {
     align-self: center;
+    font-size: 20px;
+    font-weight: 20px;
   }
 
   .img {
     align-self: center;
     width: 80%;
     height: 90%;
+    cursor: pointer;
+  }
+
+  .icon {
+    margin-left: 5px;
+    align-self: center;
+    cursor: pointer;
+    width: 50px;
+    height: 30px;
   }
 `;
 
 function Card(props) {
   let history = useHistory();
+  const [favorite, setFavorite] = useState(false);
 
   if(!props.item){
     return null
@@ -38,10 +60,20 @@ function Card(props) {
     history.push(`/${props.type}/${props.item.name}`);
   }
 
+  const handleFavClick = e => {
+    e.preventDefault();
+    setFavorite(!favorite);
+  }
+
   return (
-    <CardWrapper onClick={handleClick}>
-      <img className="img" src={props.item.photo_url} alt={props.item.name}/>
-      <div className="name">{props.item.name}</div>
+    <CardWrapper>
+      <img onClick={handleClick} className="img" src={props.item.photo_url} alt={props.item.name}/>
+      <div className="info">
+        <div className="name">{props.item.name}</div>
+        {favorite ? <FontAwesomeIcon onClick={handleFavClick} className="icon" icon={fasHeart} />
+        : <FontAwesomeIcon onClick={handleFavClick} className="icon" icon={farHeart} />
+        }
+      </div>
     </CardWrapper>
   )
 }
